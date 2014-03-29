93e8cef64b57a8766287967406a3a5506d219675
GovanifY <govanify@gmail.com>
2014-03-29 14:39:04 +0100

    Anything excepted commands
    I added anythin' 'bout the BGM format excepted for the commands(the
    cases), so the header should be here and all.
    I really hate this jscript .net btw

--- libKh/ALL/BGM.cs
+++ libKh/ALL/BGM.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.ComponentModel;
 using System.IO;
-using System.Linq.Expressions;
 
 namespace LIBKH
 {
@@ -14,18 +12,29 @@ namespace LIBKH
         {
             public void GetBGM(string nme)
             {
+                #region vars
+
                 //I really hate jscript .net(GovanifY's Rewriting BGM2MIDI in c# atm)
                 //Setting the vars(incomplete b\c I dunno at all the jscript .net language)
                 //Concept: Write in a "temp" folder the files like music066.bgm.mid or image1.imgd.bmp, for debug usage.
+                int t = 0;
+                byte cmd;
                 byte trackC;
+                var track = new byte();
                 ushort ppqn;
-                var bgmS = File.Open(nme, FileMode.Open, FileAccess.Read);
+                long tSzT;
+                int delta = 0;
+                FileStream bgmS = File.Open(nme, FileMode.Open, FileAccess.Read);
                 var bgm = new BinaryReader(bgmS);
-                var midS = File.Open(nme + ".mid", FileMode.Create, FileAccess.Write);
+                FileStream midS = File.Open(nme + ".mid", FileMode.Create, FileAccess.Write);
                 var mid = new BinaryWriter(midS);
+
+                #endregion
 
                 try
                 {
+                    #region checks&info
+
                     //Check for the debug log/console
                     if (bgm.ReadUInt32() != 0x204D4742)
                     {
@@ -35,11 +44,14 @@ namespace LIBKH
                     Console.WriteLine("Seq ID:         {0}", bgm.ReadUInt16());
                     Console.WriteLine("WD  ID:         {0}", bgm.ReadUInt16());
                     Console.WriteLine("# of Tracks:    {0}", trackC = bgm.ReadByte());
-                    Console.WriteLine("Unknown:        {0}", bgm.ReadBytes(3).ToString());
+                    Console.WriteLine("Unknown:        {0}", bgm.ReadBytes(3));
                     Console.WriteLine("In-game volume: {0}", bgm.ReadByte());
                     Console.WriteLine("Unknown2:       {0:x2}", bgm.ReadByte());
                     Console.WriteLine("PPQN:           {0}", ppqn = bgm.ReadUInt16());
                     Console.WriteLine("File-Size:      {0}", bgm.ReadUInt32());
+
+                    #endregion
+
                     bgmS.Position += 12; //padding
                     //Now writing the new midi file
 
@@ -53,17 +65,36 @@ namespace LIBKH
 
                     #endregion
 
+                    for (byte i = track; track < trackC; ++i)
+                    {
+                        tSzT = bgm.ReadUInt32();
+                        Console.WriteLine("Track {0}; Length = {1}", track, tSzT);
+                        mid.Write(0x6b72544d); //header
+                        long trackLenOffset = midS.Position;
+                        mid.Write(0x00000000); //len
+
+                        int tdelta = 0;
+                        byte channel = track;
+                        for (tSzT += bgmS.Position; bgmS.Position < tSzT - 1;)
+                        {
+                            delta = t = 0;
+                            do
+                            {
+                                delta = (delta << 7) + ((t = bgm.ReadByte()) & 0x7F);
+                            } while (t & 0x80);//I have no idea to what to do with this while
+                            tdelta += delta;
+                            cmd = bgm.ReadByte();
+
+                            #region cases commands
+
+                            #endregion
+                        }
+                    }
                 }
-                    //for(track<trackC;++track){
-
-
                 catch
                 {
                 }
-
             }
         }
-
     }
 }
-
